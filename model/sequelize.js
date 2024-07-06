4234f5b286055da138f4cf270ead601801f47ced
shadil <shadilsa786@gmai.com>
2024-07-06 14:04:32 +0530

    database correct it

--- model/sequelize.js
+++ model/sequelize.js
@@ -22,5 +22,13 @@ const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, pr
   dialect: 'postgres'
 });
 
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
 export default sequelize;
 
