7e3bfd44e065714df462b916e3f3b098f4a970f5
shadil <shadilsa786@gmai.com>
2024-07-06 14:41:25 +0530

    database changed

--- model/sequelize.js
+++ model/sequelize.js
@@ -4,18 +4,19 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const isLocal = process.env.DB_HOST === 'localhost';
-console.log(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD,process.env.DB_PORT,'process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD');
+
+console.log("DB_DATABASE:", process.env.DB_DATABASE);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
+console.log("DB_PORT:", process.env.DB_PORT);
+
 const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   dialect: 'postgres',
   dialectOptions: {
-    ssl: !isLocal ? {
-      require: true,
-      rejectUnauthorized: false
-    } : false
-  },
-  ssl: !isLocal // Some versions of Sequelize require this too
+    ssl: false 
+  }
 });
 
 // Test the database connection
