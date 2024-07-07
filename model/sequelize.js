80cfc4bfc334ff0d5e3f60c1fa9532deb0c48405
shadil <shadilsa786@gmai.com>
2024-07-07 11:25:56 +0530

    database corrected

--- model/sequelize.js
+++ model/sequelize.js
@@ -9,16 +9,21 @@ console.log("DB_DATABASE:", process.env.DB_DATABASE);
 console.log("DB_USER:", process.env.DB_USER);
 console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
 console.log("DB_PORT:", process.env.DB_PORT);
+console.log("DB_HOST:", process.env.DB_HOST);
 
 const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: parseInt(process.env.DB_PORT, 10), // Ensure the port is a number
   dialect: 'postgres',
   dialectOptions: {
-    ssl: false // Disable SSL for testing
+    ssl: isLocal ? false : { // Conditional SSL based on environment
+      require: true,
+      rejectUnauthorized: false
+    }
   },
   logging: console.log
 });
+
 // Test the database connection
 sequelize.authenticate()
   .then(() => {
